fe051143aacbf9400bb1ef3d2eb2daf10a34a6ca
mechanicalmachine <anatolyplotnikov1@gmail.com>
2020-01-14 15:45:42 +0700

    async func for flying garbage added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import random
 import time
 import curses
@@ -113,6 +114,23 @@ async def animate_spaceship(canvas, start_row, start_column, frames):
             await asyncio.sleep(0)
 
             draw_frame(canvas, row, column, frame, negative=True)
+
+
+async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
+    """Animate garbage, flying from top to bottom. Сolumn position will stay same, as specified on start."""
+
+    rows_number, columns_number = canvas.getmaxyx()
+
+    column = max(column, 0)
+    column = min(column, columns_number - 1)
+
+    row = 0
+
+    while row < rows_number:
+        draw_frame(canvas, row, column, garbage_frame)
+        await asyncio.sleep(0)
+        draw_frame(canvas, row, column, garbage_frame, negative=True)
+        row += speed
 
 
 def _get_related_filepath(dirname, filename):
